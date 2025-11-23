499661abb02e1c3ec17ac4bc4ce1413ba1ffe303
allen <allena2cool4school@gmail.com>
2025-11-23 17:06:29 -0600

    okay this one acc works

--- xlsx to html.js
+++ xlsx to html.js
@@ -1,11 +1,45 @@
-let xlsx = require('xlsx')
-let fs = require('fs')
-let wb = xlsx.readFile("output.xlsx")
-let name = wb.SheetNames[0]
-let value = wb.Sheets[name]
-let htmldata = xlsx.utils.sheet_to_html(value);
-console.log(htmldata);
-fs.writeFile("htmltable.html", htmldata, function(err){
-    if (err) {
-        return console.log(err);
-}});
+let xlsx = require('xlsx');
+let fs = require('fs');
+let INPUT_FILE = "output.xlsx";
+let OUTPUT_FILE = "htmltable.html";
+let CSS_STYLE = `
+<style>
+    table {
+        border-collapse: collapse; 
+        width: 100%;
+        font-family: Arial, Helvetica, sans-serif;
+    }
+    th, td {
+        border: 1px solid #333; 
+        padding: 8px;
+        text-align: left;
+    }
+</style>
+`;
+
+try {
+    let wb = xlsx.readFile(INPUT_FILE);
+    let sheetName = wb.SheetNames[0];
+    let ws = wb.Sheets[sheetName];
+
+    let rawTableHtml = xlsx.utils.sheet_to_html(ws);
+
+    let finalHtmlData = `
+<!DOCTYPE html>
+<html lang="en"> 
+<head>
+    <meta charset="UTF-8">
+    <title>25Live Room Reservations</title>
+    ${CSS_STYLE}
+</head>
+<body>
+    ${rawTableHtml}
+</body>
+</html>
+`;
+
+    fs.writeFileSync(OUTPUT_FILE, finalHtmlData);
+    console.log("Outputted File");
+} catch (e) {
+    console.error("Error");
+}
